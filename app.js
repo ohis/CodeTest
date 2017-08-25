24a3a2ae022ed061ff25124a4245d05f1e1ab5fe
ohis <ohifemeigbarumah1@gmail.com>
2017-08-25 06:02:54 -0500

    nothing

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-var my_module = require('./multiply');
-var ans = my_module.multiply(2,3);
-console.log(ans);
+//var my_module = require('./multiply');
+//var ans = my_module.multiply(4,3);
+//console.log(ans);
+var mult = function(num1,num2){
+  if(num1 == 0 || num2 == 0){
+    return 0;
+  }
+
+
+  else {
+    return (num1 +  mult(num1,num2-1));
+  }
+}
+
+var che = mult(5,3);
+console.log(che);
